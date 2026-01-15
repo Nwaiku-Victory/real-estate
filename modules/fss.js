736e688a4cee348a0a4675eb6690d74faa4f19d7
Nwaiku-Victory <nwaikuvictory94@gmail.com>
2026-01-14 18:24:24 -0800

    Update fss.js

--- modules/fss.js
+++ modules/fss.js
@@ -5,12 +5,12 @@ export async function fss(path) {
     try {
         let cont;
         if(fileStart == '.') {
-            cont = await fetch(`../src/${page.split('/')[0]+'/' || ''}${path.replace('./','')}`);
+            cont = await fetch(`https://nwaiku-victory.github.io/real-estate/src/${page.split('/')[0]+'/' || ''}${path.replace('./','')}`);
         } else if(fileStart == '..') {
             let pageLen = page.split('/');
-            cont = await fetch(`../src/${page.split('/')[pageLen.length-3] || ''}/${path.replace('../','')}`);
+            cont = await fetch(`https://nwaiku-victory.github.io/real-estate/src/${page.split('/')[pageLen.length-3] || ''}/${path.replace('../','')}`);
         } else {
-            cont = await fetch(`../src/${path}`);
+            cont = await fetch(`https://nwaiku-victory.github.io/real-estate/src/${path}`);
         }
         let txt;
         if(!cont.ok) {
@@ -27,6 +27,7 @@ export async function fss(path) {
     }
 
 }
+
 
 
 
