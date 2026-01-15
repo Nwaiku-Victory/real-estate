0927f9581cad34a436b7fe2623fcdff0091e92e3
Nwaiku-Victory <nwaikuvictory94@gmail.com>
2026-01-14 17:39:16 -0800

    Update fss.js

--- modules/fss.js
+++ modules/fss.js
@@ -5,12 +5,12 @@ export async function fss(path) {
     try {
         let cont;
         if(fileStart == '.') {
-            cont = await fetch(`../src/${page.split('/')[0]+'/' || ''}${path.replace('./','')}`);
+            cont = await fetch(`real-estate/src/${page.split('/')[0]+'/' || ''}${path.replace('./','')}`);
         } else if(fileStart == '..') {
             let pageLen = page.split('/');
-            cont = await fetch(`../src/${page.split('/')[pageLen.length-3] || ''}/${path.replace('../','')}`);
+            cont = await fetch(`real-estate/src/${page.split('/')[pageLen.length-3] || ''}/${path.replace('../','')}`);
         } else {
-            cont = await fetch(`../src/${path}`);
+            cont = await fetch(`real-estate/src/${path}`);
         }
         let txt;
         if(!cont.ok) {
@@ -27,4 +27,5 @@ export async function fss(path) {
     }
 
 }
+
 
